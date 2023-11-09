218e95a8b44915cd1cd41c57556db5d4e6dea6e5
Nadein Michael <nadein.michael@gmail.com>
2023-11-09 15:13:48 +0700

    ITRLT - #18 : add header banner

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,6 +6,15 @@ const Header = () => {
     <div className={"header"}>
       <div className={"container"}>
         <NavBar />
+        <div className={"header__banner"}>
+          <div className={"header__banner-wrapper"}>
+            <div className={"header__banner-info"}>
+              <p className={"header__banner-number"}>20</p>
+              <p className={"header__banner-year"}>лет</p>
+              <p className={"header__banner-text"}>Изготавливаем диваны</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
