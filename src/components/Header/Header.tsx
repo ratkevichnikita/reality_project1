2a1f065b1667d252da4490cc987a149ba644d3c8
Nadein Michael <nadein.michael@gmail.com>
2023-11-09 13:53:34 +0700

    ITRLT - #18 add NavBar component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,14 @@
+import NavBar from "../NavBar/NavBar";
+import "./styles.scss";
+
+const Header = () => {
+  return (
+    <div className={"header"}>
+      <div className={"container"}>
+        <NavBar />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
