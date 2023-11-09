61de5c5f8593a986efe53c2c800f5a14d4bd538c
Nadein Michael <nadein.michael@gmail.com>
2023-11-09 20:43:37 +0700

    ITRLT - #18 : refactoring header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,18 +1,26 @@
-import NavBar from "../NavBar/NavBar";
+import logo from "../../assets/images/logo.svg";
+import basket from "../../assets/images/basket.svg";
 import "./styles.scss";
 
 const Header = () => {
   return (
     <div className={"header"}>
       <div className={"container"}>
-        <NavBar />
-        <div className={"header__banner"}>
-          <div className={"header__banner-wrapper"}>
-            <div className={"header__banner-info"}>
-              <p className={"header__banner-number"}>20</p>
-              <p className={"header__banner-year"}>лет</p>
-              <p className={"header__banner-text"}>Изготавливаем диваны</p>
+        <div className={"header__wrapper"}>
+          <img src={logo} alt="logo" />
+          <ul className={"header__list"}>
+            <li className={"header__item header__item_active"}>Главная</li>
+            <li className={"header__item"}>Каталог мебели</li>
+            <li className={"header__item"}>О компании</li>
+            <li className={"header__item"}>Доставка и оплата</li>
+            <li className={"header__item"}>Магазины</li>
+          </ul>
+          <div className={"header__utils"}>
+            <div className={"basket"}>
+              <img className={"basket__img"} src={basket} alt="basket" />
+              <span className={"basket__counter"}>0</span>
             </div>
+            <p className={"header__phone"}>8 (917) 077-23-32</p>
           </div>
         </div>
       </div>
--- src/pages/Home/home.tsx
+++ src/pages/Home/home.tsx
@@ -1,9 +1,21 @@
 import Header from "../../components/Header/Header";
+import "./styles.scss";
 
 const Home = () => {
   return (
     <>
       <Header />
+      <div className={"container"}>
+        <div className={"banner"}>
+          <div className={"banner__wrapper"}>
+            <div className={"banner__info"}>
+              <p className={"banner__number"}>20</p>
+              <p className={"banner__year"}>лет</p>
+              <p className={"banner__text"}>Изготавливаем диваны</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
